56a3e1708bf74d79369e14357569507cd8ce586c
Feldbush <feldbush@live.ru>
2022-03-15 23:00:15 +0700

    Small fix

--- src/types/UserDataType.ts
+++ src/types/UserDataType.ts
@@ -1,9 +1,13 @@
-export interface UserDataType {
+interface UserType {
     id: number,
     parentId: number,
     isActive: boolean,
     balance: string,
     name: string,
     email: string,
+}
+
+
+export interface UserDataType extends UserType {
     childrens?: Array<UserDataType>
 }
